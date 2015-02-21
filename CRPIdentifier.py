f364d55de8135d6fbba4bd8b4209c8db3adb0055
Spencer Hong <Spencer@Spencers-MacBook-Pro-2.local>
2015-02-21 16:36:18 -0500

    Added a function for keyword making... still in development

--- CRPIdentifier.py
+++ CRPIdentifier.py
@@ -3,12 +3,16 @@ import csv
 import json
 
 from sunlight import congress
-
+from sys import argv1, argv2, argv3
 
 apikey = a07d09d6d82b4d9985b29f79c123aaec
 sunlight.config.API_KEY = apikey
 keywordspoint = 'https://congress.api.sunlightfoundation.com/'
 
-file_object = open(keyWords_Energy, r)
 
-file_object.readline
+def keyWordMaker = function(argv1, argv2, argv3)
+	nameOfSector1 = argv1
+	nameOfSector2 = argv2
+	nameOfSector3 = argv3
+	
+
