fbe9f238feab6396e74973fdbc87c6285456f9f1
Raj Shrimali <rajshrimali@gmail.com>
2015-02-21 12:28:14 -0500

    Use Sunlight's API Call instead of a csv

--- determineUnpartisanVotes.py
+++ determineUnpartisanVotes.py
@@ -1,11 +1,12 @@
-from csv import DictReader
+from sunlight import congress 
+from sunlight.pagination import PagingService
+
 class VoteLinker:
 	def __init__(self):
-		self.reader = DictReader(open ('legislators.csv', 'r'))
-	def getParty(self, bioguide):
-		for theif in self.reader:
-			if theif['bioguide_id'] == bioguide:
-				return theif['party']
-
+		paging_service = PagingService(congress)
+		self.legislators = paging_service.legislators(limit=1000)
+	def getValue(self,category, id):
+		for politician in self.legislators: 
+			if politician[category] == id: 
+				return politician 
 a = VoteLinker()
-print(a.getParty('L000304'))
