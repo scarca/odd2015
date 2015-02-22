b920db2cf5000de6ec1ab36a388e0c714192c9d9
Spencer Hong <Spencer@Spencers-MacBook-Pro-2.local>
2015-02-21 22:06:43 -0500

    Added like one line... I'm going to have to review python syntax

--- CRPIdentifier.py
+++ CRPIdentifier.py
@@ -5,14 +5,20 @@ import json
 from sunlight import congress
 from sys import argv1, argv2, argv3
 
+sector1 = argv1
+sector2 = argv2
+sector3 = argv3
+
 apikey = a07d09d6d82b4d9985b29f79c123aaec
 sunlight.config.API_KEY = apikey
 keywordspoint = 'https://congress.api.sunlightfoundation.com/'
 
 
-def keyWordMaker = function(argv1, argv2, argv3)
-	nameOfSector1 = argv1
-	nameOfSector2 = argv2
-	nameOfSector3 = argv3
+def keyWordMaker(sector):
+	#This takes three top sectors and creates three arrays of keywords
+	#um no switch statements in python.... so i'll just use multiple if-else
+	if sector === "Energy":
+		file = open(keyWords_Energy.txt)
+		
 	
 
