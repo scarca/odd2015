023ae5590d415fad193292a59a3bcdf7d3712879
Spencer Hong <Spencer@Spencers-MacBook-Pro-2.local>
2015-02-22 22:10:16 -0500

    Just added some argument variables

--- CRPIdentifier.py
+++ CRPIdentifier.py
@@ -3,11 +3,9 @@ import csv
 import json
 
 from sunlight import congress
-from sys import argv1, argv2, argv3
+from sys import argv
 
-sector1 = argv1
-sector2 = argv2
-sector3 = argv3
+script, sector1, sector2, sector3 = argv
 
 apikey = a07d09d6d82b4d9985b29f79c123aaec
 sunlight.config.API_KEY = apikey
