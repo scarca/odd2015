29bac85a88af395d4ea5fafa5bb435a143be1b18
SiddharthSrivatsan <sid.srivatsan@gmail.com>
2015-02-24 23:46:55 -0500

    Turned contribution amounts from ints to array + commented code

--- src/ContributersByIndustry.java
+++ src/ContributersByIndustry.java
@@ -1,7 +1,6 @@
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.*;
 import java.lang.Integer;
 import java.io.*;
 
@@ -70,157 +69,89 @@ public class ContributersByIndustry {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		int A = 0;
-		int B = 0;
-		int C = 0;
-		int D = 0;
-		int E = 0;
-		int F = 0;
-		int G = 0;
-		int H = 0;
-		int J11 = 0;
-		int J12 = 0;
-		int J22 = 0;
-		int J21 = 0;
-		int J51 = 0;
-		int J52 = 0;
-		int J61 = 0;
-		int J62 = 0;
-		int J712 = 0;
-		int J715 = 0;
-		int J72 = 0;
-		int J73 = 0;
-		int J74 = 0;
-		int J75 = 0;
-		int J8 = 0;
-		int JD1 = 0;
-		int JD2 = 0;
-		int JE3 = 0;
-		int JH1 = 0;
-		int Z = 0;
-		int L = 0;
-		int K12 = 0;
-		int K0 = 0;
-		int K2 = 0;
-		int M = 0;
-		int T = 0;
-		int X = 0;
-		int Y = 0;
+		int[] conts = new int[36];
+		for(int i : conts)
+		{
+			conts[i] = 0;
+		}
 		int a = 0;
 		while (a < myContribs.size()) {
 			if (myContribs.get(a).startsWith("A")) {
-				A += contribAmount.get(a);
+				conts[0] += contribAmount.get(a);
 			}
 			else if (myContribs.get(a).startsWith("B")) {
-				B += contribAmount.get(a);
+				conts[1] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("C")) {
-				C += contribAmount.get(a);
+				conts[2] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("D")) {
-				D += contribAmount.get(a);
+				conts[3] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("E")) {
-				E += contribAmount.get(a);
+				conts[4] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("F")) {
-				F += contribAmount.get(a);
+				conts[5] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("G")) {
-				G += contribAmount.get(a);
+				conts[6] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("H")) {
-				H += contribAmount.get(a);
+				conts[7] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J11")) {
-				J11 += contribAmount.get(a);
+				conts[8] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J12")) {
-				J12 += contribAmount.get(a);
+				conts[9] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J22")) {
-				J22 += contribAmount.get(a);
+				conts[10] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J21")) {
-				J21 += contribAmount.get(a);
+				conts[11] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J51")) {
-				J51 += contribAmount.get(a);
+				conts[12] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J52")) {
-				J52 += contribAmount.get(a);
+				conts[13] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J61")) {
-				J61 += contribAmount.get(a);
+				conts[14] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J62")) {
-				J62 += contribAmount.get(a);
+				conts[15] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J712")) {
-				J712 += contribAmount.get(a);
+				conts[16] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J715")) {
-				J715 += contribAmount.get(a);
+				conts[17] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J72")) {
-				J72 += contribAmount.get(a);
+				conts[18] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J73")) {
-				J73 += contribAmount.get(a);
+				conts[19] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J74")) {
-				J74 += contribAmount.get(a);
+				conts[20] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J75")) {
-				J75 += contribAmount.get(a);
+				conts[21] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("J8")) {
-				J8 += contribAmount.get(a);
+				conts[22] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("JD1")) {
-				JD1 += contribAmount.get(a);
+				conts[23] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("JD2")) {
-				JD2 += contribAmount.get(a);
+				conts[24] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("JE3")) {
-				JE3 += contribAmount.get(a);
+				conts[25] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("JH1")) {
-				JH1 += contribAmount.get(a);
+				conts[26] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("Z")) {
-				Z += contribAmount.get(a);
+				conts[27] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("L")) {
-				L += contribAmount.get(a);
+				conts[28] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("K12")) {
-				K12 += contribAmount.get(a);
+				conts[29] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("K0")) {
-				K0 += contribAmount.get(a);
+				conts[30] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("K2")) {
-				K2 += contribAmount.get(a);
+				conts[31] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("M")) {
-				M += contribAmount.get(a);
+				conts[32] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("T")) {
-				T += contribAmount.get(a);
+				conts[33] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("X")) {
-				X += contribAmount.get(a);
+				conts[34] += contribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("Y")) {
-				Y += contribAmount.get(a);
+				conts[35] += contribAmount.get(a);
 			}
 			a++;
 		}
-		int[] conts = new int[36];
-		conts[0] = A;
-		conts[1] = B;
-		conts[2] = C;
-		conts[3] = D;
-		conts[4] = E;
-		conts[5] = F;
-		conts[6] = G;
-		conts[7] = H;
-		conts[8] = J11;
-		conts[9] = J12;
-		conts[11] = J21;
-		conts[10] = J22;
-		conts[12] = J51;
-		conts[13] = J52;
-		conts[14] = J61;
-		conts[15] = J62;
-		conts[16] = J712;
-		conts[17] = J715;
-		conts[18] = J72;
-		conts[19] = J73;
-		conts[20] = J74;
-		conts[21] = J75;
-		conts[22] = J8;
-		conts[23] = JD1;
-		conts[24] = JD2;
-		conts[25] = JE3;
-		conts[26] = JH1;
-		conts[27] = K0;
-		conts[28] = K12;
-		conts[29] = K2;
-		conts[30] = L;
-		conts[31] = M;
-		conts[32] = T;
-		conts[33] = X;
-		conts[34] = Y;
-		conts[35] = Z;
 		return conts;
 	}
 	public static void main(String[] args) {
@@ -257,10 +188,6 @@ public class ContributersByIndustry {
 		codes.remove(0);
 		industry.remove(0);
 		// User enters the location of the CRP code file
-		// Scanner input2 = new Scanner(System.in);
-		// System.out.println("Enter CRP code location: ");
-		// System.out.println("---Example: \"src/CRPs_IDs.csv\"---");
-		// String s2 = input2.next();
 		File crp = new File("src/CRPs_IDs.csv");
 		// Creates ArrayLists for the CRP IDs and their corresponding
 		// congresspeople
@@ -293,10 +220,6 @@ public class ContributersByIndustry {
 			names.set(i, names.get(i).substring(5, names.get(i).length() - 1));
 		}
 		// User enters the location of the targeted congress people file
-		// Scanner input3 = new Scanner(System.in);
-		// System.out.println("Enter targeted congresspeople location: ");
-		// System.out.println("---Example: \"src/CongressPeopleCRP.csv\"---");
-		// String s3 = input3.next();
 		File CRP = new File("src/CongressPeopleCRP.csv");
 		// Creates an ArrayList to hold the CRP IDs of the targeted
 		// congresspeople
@@ -307,10 +230,13 @@ public class ContributersByIndustry {
 			while (scanner.hasNext()) {
 				congressCRPs.add(scanner.next());
 			}
+			scanner.close();
 		}
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+		//Adds the names of the chosen congresspeople CRPs to an ArrayList
 		int i = 0;
 		int x = 0;
 		List<String> chosenPeople = new ArrayList<String>();
@@ -322,9 +248,12 @@ public class ContributersByIndustry {
 			}
 			i++;
 		}
+		//Prints out the names of the chosen congresspeople
 		for (String z : chosenPeople) {
 			System.out.println(z);
 		}
+		/*Adds the contribution amount by industry for the previously selected congresspeople to a 
+		two-dimensional array by repeatedly calling the method defined prior to the main method*/
 		int[][] conts = new int[congressCRPs.size()][36];
 		for (int h = 0; h < congressCRPs.size(); h++) {
 			for (int p = 0; p < 36; p++) {
@@ -332,13 +261,14 @@ public class ContributersByIndustry {
 						.get(h))[p];
 			}
 		}
-		int l = 0;
+		//Prints out contribution amounts by industry for each of the selected congresspeople
 		for (int t = 0; t < congressCRPs.size(); t++) {
 			for (int e = 0; e < 36; e++) {
 				System.out.print(industry.get(e) + ": ");
 				System.out.println(conts[t][e]);
 			}
 		}
+		//Writes the contribution by industry data to a csv file
 		try {
 			PrintWriter writer = new PrintWriter(
 					"src/CongresspeopleContribs.csv");
