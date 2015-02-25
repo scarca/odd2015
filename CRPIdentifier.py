4d428cbb00b4e19aab112cb5fe7f9e76817482d0
Spencer Hong <Spencer@Spencers-MacBook-Pro-2.local>
2015-02-24 23:31:25 -0500

    Added Pseudo code.. this will help me code later

--- CRPIdentifier.py
+++ CRPIdentifier.py
@@ -12,6 +12,42 @@ sunlight.config.API_KEY = apikey
 keywordspoint = 'https://congress.api.sunlightfoundation.com/'
 
 
+
+'''
+okay, this is pseudo code before I write actually my code. 
+Goal: 
+1st, find the sectors that x candidate received most money from.
+2nd, find the bills that closely relate to those sectors.
+
+
+
+functions:
+
+a function that will take the three most sectors, and correlate them with a 'list' of keywords from that sector
+i.e.
+Energy
+Construction
+Environment
+
+*** energy = [keyword1, keyword2, keyword3]
+*** construction = [keyword1, keyword2, keyword3]
+*** environment = [keyword1, keyword2, keyword2]
+
+Then, go to the online API, and look for bills that match those keywords.
+This function will also allow for parameters of last added, and _____ (add this in the morning)
+also, reminder to myself to add raj's code in here in the morning.
+
+
+
+
+'''
+
+
+
+
+
+
+
 def billFinder(keyword, party):
 	#connects to the sunlight foundation API and finds the bill keywords
 	 
