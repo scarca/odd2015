546c63b48eaed884eb2f1ddeb1695aa8671c279b
Raj Shrimali <rajshrimali@gmail.com>
2015-02-26 17:31:38 -0500

    Initializing Node
    Includes all required modules, except express

--- index.js
+++
@@ -1,9 +0,0 @@
-var children = require('child_process')
-var http = require('http')
-
-var handle = function(request, response){
-
-}
-
-var serv = http.createServer(handle)
-serv.listen(10000)
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Campaign Clarity', Message:'hi' });
+});
+
+module.exports = router;
