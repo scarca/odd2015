569a132ac81eb914f2358b0485c300189d73f5cc
Raj Shrimali <rajshrimali@gmail.com>
2015-02-27 13:57:32 -0500

    Use command-line arguments
    Replace the prompts with a command-line argument parser so that we can
    run this from other programs.

--- CRPIdentifier.py
+++ CRPIdentifier.py
@@ -8,7 +8,7 @@ pag = PagingService(congress)
 
 
 
-def keyWordMaker():
+def keyWordMaker(sector, chamber):
 	#This takes three top sectors and creates three arrays of keywords
 	#um no switch statements in python.... so i'll just use multiple if-else
 	#Open up json
@@ -18,9 +18,6 @@ def keyWordMaker():
 	#access each
 	for key, value in kw.items():
 		print(key, value)
-	sector = prompt("Please choose a sector that corresponds with the keywords that you want: ")
-
-	chamber = prompt("please choose the chamber that the bill originated from: ")
 
 	enaction = "";
 	if chamber.Lower() == "house":
@@ -31,3 +28,30 @@ def keyWordMaker():
 		print "error: the chamber input did not exist"
 	bill_list = list(pag.congress.search_bills(keywords=sector, chamber = chamber, enactionHistory = "pass", fields=official_title, summary, bill.id, limit = 1000000))
 	print (bill_list)
+
+if __name__ == "__main__":
+	global sector, chamber
+	sector = ""
+	chamber = ""
+	def action(verb, arg):
+		if verb == "sector" or verb == "s":
+			global sector
+			sector = arg
+			return chamber != ""
+		if verb == "chamber" or verb =="c":
+			global chamber
+			chamber = arg
+			return sector != ""
+
+	for arg in sys.argv:
+		nextArg = (False, "")
+		done = False
+		if arg[0] == "-":
+			nextArg[0] = True
+			nextArg[1] = arg[1:]
+		elif nextArg[0]:
+			act = arg
+			done = action(nextArg[1], arg)
+		if done:
+			keyWordMaker(sector, chamber)
+			break;
