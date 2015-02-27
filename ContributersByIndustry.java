bc3611b40df6eca352418d1068a993230fa76137
SiddharthSrivatsan <sid.srivatsan@gmail.com>
2015-02-26 21:21:04 -0500

    Now runnable when the entire odd2015 folder is downloaded

--- ContributersByIndustry.java
+++ ContributersByIndustry.java
@@ -3,7 +3,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.Integer;
 import java.io.*;
-public class ContributersByIndustry implements Runnable{
+
+public class ContributersByIndustry implements Runnable {
 	private static List<String> allContributions = new ArrayList<String>();
 	private static List<String> CRPs = new ArrayList<String>();
 	private static List<String> myContribs = new ArrayList<String>();
@@ -16,42 +17,46 @@ public class ContributersByIndustry implements Runnable{
 	private static List<String> chosenPeople = new ArrayList<String>();
 	private static List<String> codes = new ArrayList<String>();
 	private static List<String> industry = new ArrayList<String>();
-	
 	public void run() {
-				// User enters the location of the sector codes file
-				File sectorCodes = new File("src/Sector_Codes.csv");
-				// Adds sector codes and industries to their respective ArrayLists
-				try {
-					Scanner scan = new Scanner(sectorCodes);
-					scan.useDelimiter(",");
-					boolean pick = true;
-					while (scan.hasNext()) {
-						if (pick == true) {
-							codes.add(scan.next());
-							pick = false;
-						}
-						else {
-							industry.add(scan.next());
-							pick = true;
-						}
-					}
-					scan.close();
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
+		// Location of the sector codes file
+		File sectorCodes = new File("src/src/Sector_Codes.csv");
+		// Adds sector codes and industries to their respective ArrayLists
+		try {
+			Scanner scan = new Scanner(sectorCodes);
+			scan.useDelimiter(",");
+			boolean pick = true;
+			while (scan.hasNext()) {
+				if (pick == true) {
+					codes.add(scan.next());
+					pick = false;
+				} else {
+					industry.add(scan.next());
+					pick = true;
 				}
-				// Removes the header in the CSV from both ArrayLists
-				codes.remove(0);
-				industry.remove(0);
+			}
+			scan.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		// Removes the header in the CSV from both ArrayLists
+		codes.remove(0);
+		industry.remove(0);
 	}
-	
 	public static int[] contributors(String s) {
+		// Clears previous entries in candidate-specific arraylists
 		myContribs.clear();
 		myContribAmount.clear();
-		File contribs = new File("src/AllContribs.csv");
-		File contribs2 = new File("src/ContributionsAmounts.csv");
+		// Location of file containing all contributions
+		File contribs = new File("src/src/AllContribs.csv");
+		// Location of file containing all contribution amounts
+		File contribs2 = new File("src/src/ContributionsAmounts.csv");
 		try {
-			if(allContributions.isEmpty())
-			{
+			/*
+			 * If the arraylist containing all the contributions is empty, then
+			 * it will add the contributions from the contributions file
+			 */
+			if (allContributions.isEmpty()) {
+				// Adds contributions from file to arraylist
 				Scanner myScanner = new Scanner(contribs);
 				myScanner.useDelimiter(",");
 				boolean choose = true;
@@ -59,25 +64,34 @@ public class ContributersByIndustry implements Runnable{
 					if (choose == true) {
 						myScanner.next();
 						choose = false;
-					}
-					else {
+					} else {
 						allContributions.add(myScanner.nextLine());
 						choose = true;
 					}
 				}
 				myScanner.close();
+				// Removes header from the csv file that got transferred to the
+				// arraylist
 				allContributions.remove(0);
+				// Removes unnecessary commas from arraylist containing all
+				// contributions
 				for (int r = 0; r < allContributions.size(); r++) {
-					if(allContributions.get(r).length() > 4)
-						allContributions.set(r, allContributions.get(r).substring(1));
+					if (allContributions.get(r).length() > 4)
+						allContributions.set(r, allContributions.get(r)
+								.substring(1));
 					else {
 						allContributions.remove(r);
 						r--;
 					}
 				}
 			}
-			if(CRPs.isEmpty())
-			{
+			/*
+			 * If the arraylist containing all the CRP codes for contribution
+			 * recipients is empty, then it will be added from the contributions
+			 * file
+			 */
+			if (CRPs.isEmpty()) {
+				// Adds CRPs for contributions from file
 				Scanner myScanner2 = new Scanner(contribs);
 				myScanner2.useDelimiter(",");
 				boolean choose2 = true;
@@ -85,50 +99,59 @@ public class ContributersByIndustry implements Runnable{
 					if (choose2 == true) {
 						CRPs.add(myScanner2.next());
 						choose2 = false;
-					}
-					else {
+					} else {
 						myScanner2.nextLine();
 						choose2 = true;
 					}
 				}
+				// Removes header from csv that got transferred to the arraylist
 				CRPs.remove(0);
 				myScanner2.close();
 			}
-			if(contribAmountStrings.isEmpty()) {
+			/*
+			 * If the arraylist containing the contribution amounts is empty,
+			 * then it will be added from the contribution amounts file
+			 */
+			if (contribAmountStrings.isEmpty()) {
+				// Adds contribution amounts to arraylist from file
 				Scanner scan3 = new Scanner(contribs2);
 				while (scan3.hasNext()) {
 					contribAmountStrings.add(scan3.nextLine());
 				}
 				scan3.close();
+				// Removes header transferred from csv file to the arraylist
 				contribAmountStrings.remove(0);
+				// Converts the contribution amounts from strings to integers,
+				// and adds them to a new arraylist
 				for (int l = 0; l < contribAmountStrings.size(); l++) {
-					contribAmount
-							.add(Integer.parseInt(contribAmountStrings.get(l)));
+					contribAmount.add(Integer.parseInt(contribAmountStrings
+							.get(l)));
 				}
 			}
-				int i = 0;
-				while (i < CRPs.size()) {
-					if (CRPs.get(i).equals(s)) {
-						myContribs.add(allContributions.get(i));
-						myContribAmount.add(contribAmount.get(i));
-					}
-					i++;
+			//Places the contributions and contribution amounts corresponding to the chosen candidates
+			//in it's own arraylists
+			int i = 0;
+			while (i < CRPs.size()) {
+				if (CRPs.get(i).equals(s)) {
+					myContribs.add(allContributions.get(i));
+					myContribAmount.add(contribAmount.get(i));
 				}
-			
+				i++;
+			}
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		//Adds up the total contributions in dollars by sector to the chosen candidates
 		int[] conts = new int[36];
-		for(int i : conts)
-		{
+		for (int i : conts) {
 			conts[i] = 0;
 		}
 		int a = 0;
 		while (a < myContribs.size()) {
 			if (myContribs.get(a).startsWith("A")) {
 				conts[0] += myContribAmount.get(a);
-			}
-			else if (myContribs.get(a).startsWith("B")) {
+			} else if (myContribs.get(a).startsWith("B")) {
 				conts[1] += myContribAmount.get(a);
 			} else if (myContribs.get(a).startsWith("C")) {
 				conts[2] += myContribAmount.get(a);
@@ -203,11 +226,10 @@ public class ContributersByIndustry implements Runnable{
 		}
 		return conts;
 	}
-
 	public static void main(String[] args) {
 		(new Thread(new ContributersByIndustry())).start();
 		// User enters the location of the CRP code file
-		File crp = new File("src/CRPs_IDs.csv");
+		File crp = new File("src/src/CRPs_IDs.csv");
 		// Adds CRP IDs and congresspeople to their respective ArrayLists
 		try {
 			Scanner scanner = new Scanner(crp);
@@ -217,8 +239,7 @@ public class ContributersByIndustry implements Runnable{
 				if (pick == true) {
 					IDs.add(scanner.next());
 					pick = false;
-				}
-				else {
+				} else {
 					names.add(scanner.nextLine());
 					pick = true;
 				}
@@ -235,7 +256,7 @@ public class ContributersByIndustry implements Runnable{
 			names.set(i, names.get(i).substring(5, names.get(i).length() - 1));
 		}
 		// User enters the location of the targeted congress people file
-		File CRP = new File("src/CongressPeopleCRP.csv");
+		File CRP = new File("src/transfer.txt");
 
 		// Adds the CRP codes of the targeted congress people to the ArrayList
 		try {
@@ -244,12 +265,11 @@ public class ContributersByIndustry implements Runnable{
 				congressCRPs.add(scanner.next());
 			}
 			scanner.close();
-		}
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-		//Adds the names of the chosen congresspeople CRPs to an ArrayList
+
+		// Adds the names of the chosen congresspeople CRPs to an ArrayList
 		int i = 0;
 		int x = 0;
 		while (i < IDs.size() && x < congressCRPs.size()) {
@@ -260,12 +280,15 @@ public class ContributersByIndustry implements Runnable{
 			}
 			i++;
 		}
-		//Prints out the names of the chosen congresspeople
+		// Prints out the names of the chosen congresspeople
 		for (String z : chosenPeople) {
 			System.out.println(z);
 		}
-		/*Adds the contribution amount by industry for the previously selected congresspeople to a 
-		two-dimensional array by repeatedly calling the method defined prior to the main method*/
+		/*
+		 * Adds the contribution amount by industry for the previously selected
+		 * congresspeople to a two-dimensional array by repeatedly calling the
+		 * method defined prior to the main method
+		 */
 		int[][] conts = new int[congressCRPs.size()][36];
 		for (int h = 0; h < congressCRPs.size(); h++) {
 			for (int p = 0; p < 36; p++) {
@@ -273,14 +296,15 @@ public class ContributersByIndustry implements Runnable{
 						.get(h))[p];
 			}
 		}
-		//Prints out contribution amounts by industry for each of the selected congresspeople
+		// Prints out contribution amounts by industry for each of the selected
+		// congresspeople
 		for (int t = 0; t < congressCRPs.size(); t++) {
 			for (int e = 0; e < 36; e++) {
 				System.out.print(industry.get(e) + ": ");
 				System.out.println(conts[t][e]);
 			}
 		}
-		//Writes the contribution by industry data to a csv file
+		// Writes the contribution by industry data to a csv file
 		try {
 			PrintWriter writer = new PrintWriter(
 					"src/CongresspeopleContribs.csv");
