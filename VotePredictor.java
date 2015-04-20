6ef0173316bf3a5602b61308d024a5032d210a57
SiddharthSrivatsan <sid.srivatsan@gmail.com>
2015-04-20 00:14:29 -0400

    Added comments

--- VotePredictor.java
+++ VotePredictor.java
@@ -46,31 +46,39 @@ public class VotePredictor {
 		 */
 		for(Map.Entry<String, HashMap<String, Integer>> entry : incomeByDistrict.entrySet())
 		{
+			//Temporarily holds current value of HashMap being iterated over
 			HashMap<String, Integer> temp = entry.getValue();
 			ArrayList<Integer> incomes = new ArrayList<>();
+			
+			//Adds each of the household income numbers to an ArrayList
 			for(Map.Entry<String, Integer> entry2 : temp.entrySet())
 			{
 				incomes.add(entry2.getValue());
 			}
 			
+			//Sorts household income numbers into three bracekts: poor, middle, and rich
 			int poorHouseholds = incomes.get(0) + incomes.get(1) + incomes.get(2);
 			int middleHouseholds = incomes.get(3) + incomes.get(4) + incomes.get(5);
 			int richHouseholds = incomes.get(6) + incomes.get(7) + incomes.get(8) + incomes.get(9);
 			
+			//The total number of households in the district
 			int total = poorHouseholds + middleHouseholds + richHouseholds;
 			
+			//Calculates the percent of poor, middle, and rich households in the district
 			int poorPercent = poorHouseholds / total;
 			int middlePercent = middleHouseholds / total;
 			int richPercent = richHouseholds / total;
 			
+			//Finds the bracket that makes up the largest percentage of the district
 			double max = (poorPercent > middlePercent) ? poorPercent : middlePercent;
 			max = (max > richPercent) ? max : richPercent;
 			
+			//Weighs a district with mostly middle class as 50-50 conservative-liberal
 			if(max == middlePercent)
 			{
 				max = 0.5;
 			}
-			
+			//Weighs a district with mostly poor people as leaning towards liberal
 			else if(max == poorPercent)
 			{
 				max = 0.5 + poorPercent;
@@ -80,6 +88,7 @@ public class VotePredictor {
 				}
 			}
 			
+			//Adds weight to the HashMap with the income weights
 			incomeWeights.put(entry.getKey(), max);
 			
 		}
