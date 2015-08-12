387d8ffdcc62d0255507d1474058b90682a25b9f
Raj Shrimali <scarca@protonmail.ch>
2015-08-12 18:48:57 -0400

    Use bl to combine all data in KW https req
    The code was breaking on slower internet connections because it would try to parse the JSON before all the data had finished coming from the https.get() request. So, use a bl to combine it all into one first.

--- bin/comm.js
+++ bin/comm.js
@@ -3,18 +3,19 @@ module.exports = function(server){
     var handler = require('./handler')
     var csv = require('csv')
     var https = require('https')
+	var bl = require('bl');
     io.on('error', function(err){
         console.log(err);
     });
 
     io.on('connection', function(socket){
         var emitStatus = function(data){
-	    console.log(data)
+		    console.log(data)
             socket.emit("update", {status: data});
         }
         var handle = function(bill){
             handler.startPy([[root + "/VoteLinker.py", "-v", bill]],emitStatus, function(code, data){
-		console.log("Started Python")
+			console.log("Started Python")
                 if(code == 2){
                     var datum = {err: "Bill Does Not Exist"}
                     socket.emit("err", datum)
@@ -38,17 +39,21 @@ module.exports = function(server){
             console.log("KW:",kw['s'])
             kw['s'].replace(" ", "%20");
             https.get('https://congress.api.sunlightfoundation.com/bills/search?query='+kw['s']+'&apikey=a07d09d6d82b4d9985b29f79c123aaec&fields=bill_id&', function(res){
-                res.on('data', function(data){
+				res.pipe(bl(function(err, data){
+              //  res.on('data', function(data){
+					console.log("Received data:", data); 
                     var j = JSON.parse(data)
+					console.log("[DEBUG] FINISHED PARSE"); 
                     if(j['count'] == 0){
-			console.log("No Bill Found!");
+						console.log("No Bill Found!");
                         var datum = {'err': "No Bill Found!"}
                         socket.emit('err', datum)
                     }
                     else{
+						console.log("Found bill: ", j['results'])
                         handle(j['results'][0]['bill_id'])
                     }
-                })
+                }))
             })
         })
     })
